d81672b2c73d93774e935d3aeef880db4fd1f9a4
Sophie Gabriela Laksø <slak@itu.dk>
2024-09-03 11:04:46 +0200

    gitignore file has been added, main has been refor mated

--- handin1.go
+++ handin1.go
@@ -4,27 +4,56 @@
 package main
 
 func main() {
-	go fork()
 	//EACH fork must have a go routine
-	go philo()
 	//EACH philo must have a go routine
 }
 
-//do we need a func for each fork and each philo?
+func hollenborg() {
+	//request forks
+
+	//print for every phil if they're eating or thinking
+	//^ if !notEating : thinking
+}
+
+func laksoe() {
+
+}
+
+func sandberg() {
+
+}
+
+func decarte() {
+
+}
+
+func niet() { //nietzsche
+
+}
 
 func fork() {
 	//fork-array? 5 spots, each spot is a corresponding table-placement?
 	//or 10 spots, every second empty, used by a parallel philos array
 }
 
+func prongs() {
+
+}
+
+func trifork() {
+
+}
+
+func pick() {
+
+}
+
+func spork() {
+
+}
+
 //arrays should be SEPERATE, or else fork and philos might change the array at
 //the same time and fuck it UP
-func philos() {
-	//request forks
-
-	//print for every phil if they're eating or thinking
-	//^ if !notEating : thinking
-}
 
 //use lock and unlock? lock forks when in use, unlock when not in use
 //forks and philos need to communicate with channels
