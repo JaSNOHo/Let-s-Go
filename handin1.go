9b026524dfe5ecbe263f393a7ee74cde2ca779ef
Sophie Gabriela Laksø <slak@itu.dk>
2024-09-03 11:49:15 +0200

    logic comments

--- handin1.go
+++ handin1.go
@@ -4,52 +4,85 @@
 package main
 
 func main() {
-	//EACH fork must have a go routine
-	//EACH philo must have a go routine
+	table := make([]func(), 10)
+	table[0] = hollenborg
+	table[1] = fork
+	table[2] = laksoe
+	table[3] = pick
+	table[4] = sandberg
+	table[5] = prongs
+	table[6] = descartes
+	table[7] = trifork
+	table[8] = niet
+	table[9] = spork
+
+	go hollenborg()
+	go laksoe()
+	go sandberg()
+	go descartes()
+	go niet()
+	go fork()
+	go pick()
+	go prongs()
+	go trifork()
+	go pick()
+	go spork()
 }
 
-func hollenborg() {
+func hollenborg() { //[0]
+	var mealAmt int
 	//request forks
-
 	//print for every phil if they're eating or thinking
 	//^ if !notEating : thinking
+	if !table[9].isHeld && !table[1].isHeld {
+		LOCK
+		forks[9].isHeld = true
+		forks[1].isHeld = true
+		print("Hollenborg is eating")
+		mealAmt++
+		forks[9].isHeld = false
+		forks[1].isHeld = false
+		UNLOCK
+	} else {
+		print("Hollenborg is thinking")
+	}
 }
 
-func laksoe() {
-
+func laksoe() { //[2]
+	//
 }
 
-func sandberg() {
-
-}
-
-func decarte() {
-
-}
-
-func niet() { //nietzsche
-
-}
-
-func fork() {
-	//fork-array? 5 spots, each spot is a corresponding table-placement?
-	//or 10 spots, every second empty, used by a parallel philos array
-}
-
-func prongs() {
+func sandberg() { //[4]
 
 }
 
-func trifork() {
+func descartes() { //[6]
 
 }
 
-func pick() {
+func niet() { //nietzsche [8]
 
 }
 
-func spork() {
+func fork() { //[1]
+	var isHeld bool
 
+}
+
+func prongs() { //[3]
+	var isHeld bool
+}
+
+func trifork() { //[5]
+	var isHeld bool
+}
+
+func pick() { //[7]
+	var isHeld bool
+}
+
+func spork() { //[9]
+	var isHeld bool = false
 }
 
 //arrays should be SEPERATE, or else fork and philos might change the array at
